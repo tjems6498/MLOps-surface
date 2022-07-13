e42736d217535b179bf04026ba068c7bec9137b7
tjems6498 <tjems6498@gmail.com>
2022-07-13 11:52:49 +0900

    edit serve

--- serve/mlflow_model.py
+++ serve/mlflow_model.py
@@ -6,11 +6,11 @@ from mlflow.tracking import MlflowClient
 def load_model(model_name, version):
     os.environ["AWS_ACCESS_KEY_ID"] = "minio"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
-    # os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://minio-service.kubeflow.svc:9000"
-    # client = MlflowClient("http://mlflow-server-service.mlflow-system.svc:5000")
+    os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://minio-service.kubeflow.svc:9000"
+    client = MlflowClient("http://mlflow-server-service.mlflow-system.svc:5000")
 
-    os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://116.47.188.227:31968"
-    client = MlflowClient("http://116.47.188.227:30842")
+    # os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://116.47.188.227:31968"
+    # client = MlflowClient("http://116.47.188.227:30842")
 
     filter_string = f"name='{model_name}'"
     results = client.search_model_versions(filter_string)  # 버전별로 따로 나옴
@@ -20,7 +20,6 @@ def load_model(model_name, version):
         if res.version == str(version):
             model_uri = res.source
             break
-    print(model_uri)
     reconstructed_model = mlflow.pytorch.load_model(model_uri)
     return reconstructed_model
 
