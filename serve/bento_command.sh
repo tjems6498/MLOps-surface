af047d76881dd07f383c58b55bf29f0ed51215a6
tjems6498 <tjems6498@gmail.com>
2022-07-14 14:48:49 +0900

    serve file

--- serve/bento_command.sh
+++ serve/bento_command.sh
@@ -2,6 +2,6 @@
 
 TOKEN=$1
 
-bentoml yatai login --api-token $TOKEN --endpoint "http://116.47.188.227:30080"
+bentoml yatai login --api-token=$TOKEN --endpoint=http://116.47.188.227:30080
 bentoml build
 bentoml push surface_clf:latest
