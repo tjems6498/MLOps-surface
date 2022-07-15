bba3075d3ee620257df06c73da55e44ff55cae16
tjems6498 <tjems6498@gmail.com>
2022-07-15 09:23:56 +0900

    serve file

--- serve/service.py
+++ serve/service.py
@@ -14,7 +14,7 @@ SURFACE_CLASSES = ['negatieve', 'positive']
 
 
 surface_clf_runner = bentoml.pytorch.get("surface_clf:latest").to_runner()
-svc = bentoml.Service("surface_classification", runners=[surface_clf_runner])
+svc = bentoml.Service("surface_convnext", runners=[surface_clf_runner])
 
 @svc.api(input=Image(), output=NumpyNdarray())
 def classify(imgs):
